4490d8fd612949ebdcef4d538453722b8ea5d124
Lee Jihee <129031670+munjji@users.noreply.github.com>
2024-11-13 13:05:27 +0900

    feat: input 태그 엑셀만 제출할 수 있도록 수정

--- src/components/chart/UploadButton.jsx
+++ src/components/chart/UploadButton.jsx
@@ -4,7 +4,7 @@ import file from '../../assets/images/file.svg';
 const UploadButton = () => {
   return (
     <label className="w-[151px] h-[50px] rounded-[20px] bg-brown-100/60 flex items-center justify-center cursor-pointer">
-      <input type="file" className="hidden" />
+      <input type="file" className="hidden" accept=".xls,.xlsx,.xlsm" />
       <img src={file} alt="파일 아이콘" />
       <p className="text-xl text-[#ffff] text-center tracking-[-2.1px] ml-[5px]">파일 올리기</p>
     </label>
