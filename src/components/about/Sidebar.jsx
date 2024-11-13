8db9f0bad900a7bd2706ae16d4249a661d43ce1c
Lee Jihee <129031670+munjji@users.noreply.github.com>
2024-11-13 10:59:03 +0900

    refactor: 필요없는 라이브러리 삭제

--- src/components/about/Sidebar.jsx
+++ src/components/about/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import acoFace from '../../assets/images/aco_face.svg';
 import home from '../../assets/images/home.svg';
