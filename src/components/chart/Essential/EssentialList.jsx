196e2e6bf13621ae3b5f6334c4a1ccfd9b5fcbeb
Lee Jihee <129031670+munjji@users.noreply.github.com>
2024-11-14 11:07:42 +0900

    feat: 필수 강의 수강여부를 통해 받아온 값 표현

--- src/components/chart/Essential/EssentialList.jsx
+++ src/components/chart/Essential/EssentialList.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ClassDiv from './ClassDiv';
 
-const EssentialList = ({ name }) => {
+const EssentialList = ({ name, subjects }) => {
   return (
     <div className="flex flex-col gap-[17px]">
       <p className="text-[26px] font-semibold text-brown-100 tracking-[-2.1px]">{name}</p>
-      <ClassDiv />
+      {subjects.map(subject => (
+        <ClassDiv
+          key={subject.subjectName}
+          is={subject.isCompleted}
+          subjectName={subject.subjectName}
+        />
+      ))}
     </div>
   );
 };
